35279ee4bce81996e61796b6ea31fdfcf7aff78a
song <ssogg5309@naver.com>
2024-12-09 05:07:47 +0900

    feat: 계층 권한 설정

--- src/main/java/com/example/auth/config/SecurityConfig.java
+++ src/main/java/com/example/auth/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.example.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,13 +19,21 @@ public class SecurityConfig {
 	}
 
 	@Bean
+	public RoleHierarchy roleHierarchy() {
+		return RoleHierarchyImpl.withDefaultRolePrefix()
+			.role("ADMIN").implies("MANAGER")
+			.role("MANAGER").implies("USER")
+			.build();
+	}
+
+	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
 		http
 			.authorizeHttpRequests((auth) -> auth
 				.requestMatchers("/", "/login", "/loginProc", "/join", "/joinProc").permitAll()
 				.requestMatchers("/admin").hasRole("ADMIN")
-				.requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
+				.requestMatchers("/my/**").hasAnyRole("USER")
 				.anyRequest().authenticated()
 			);
 
